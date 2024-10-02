9651f3e2b3af2b3fe3f7e64ad8693bad9c509f92
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-10-02 13:56:39 +0200

    start of portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Tairou`s Portfolio</h1>
+      <p> Coming Soon</p>
+
+      <footer>
+        <p>© 2024 Tairou</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
